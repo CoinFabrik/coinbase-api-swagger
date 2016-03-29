7d76f0cc03c43e9a70b15a645f3963d54daa607e
ferni <fertiberti@gmail.com>
2016-03-29 16:28:15 -0300

    Users "put" test.

--- test/users.js
+++ test/users.js
@@ -17,4 +17,31 @@ describe('/user', function() {
       });
     });
   });
+  describe('put', function () {
+    it('should update the user\'s name', function (done) {
+      function getRandomName() {
+        var letters = 'abcdefghijklnmopqrstuvwxyz',
+          name = '';
+        for  (var i = 0; i < 10; i++) {
+          name = name + letters[Math.floor(Math.random() * (letters.length - 1))];
+        }
+        return name;
+      }
+      var name = getRandomName();
+      console.log(name);
+      var opts = {
+         body: {name: name} // {Body} Properties to update
+      };
+      usersApi.userPut(opts, function(error, data, response) {
+        if (error) {
+          console.log(error.text);
+        }
+
+        should.not.exist(error);
+        response.statusCode.should.equal(200);
+        data.data.name.should.equal(name);
+        done();
+      });
+    });
+  });
 });
