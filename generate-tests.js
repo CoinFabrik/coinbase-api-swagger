2a7095d913f8a4f13e39463fe5353ba4ccdfc0da
ferni <fertiberti@gmail.com>
2016-04-13 18:31:14 -0300

    Generate tests using swagger-test-templates

---
+++ generate-tests.js
@@ -0,0 +1,18 @@
+var stt = require('swagger-test-templates'),
+  swagger = require('./swagger.json'),
+  fs = require('fs');
+var config = {
+  assertionFormat: 'should',
+  testModule: 'supertest',
+  pathName: Object.keys(swagger.paths),
+  maxLen: 80
+};
+//Fernando's User ID (sandbox) : "891ae771-ce5a-5014-801b-5461b5481e80"
+
+
+// Generates an array of JavaScript test files following specified configuration 
+var files = stt.testGen(swagger, config);
+console.log(files);
+files.forEach(file => {
+  fs.writeFileSync('./test/built-tests/' + file.name, file.test);
+});
