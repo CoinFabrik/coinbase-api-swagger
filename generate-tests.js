1d9a1d82f7a372f489807d4d2c36a5e73fd1c501
ferni <fertiberti@gmail.com>
2016-04-19 04:03:16 -0300

    Use json-schema-deref to solve references before test generation.

--- generate-tests.js
+++ generate-tests.js
@@ -1,6 +1,8 @@
 var stt = require('swagger-test-templates'),
-  swagger = require('./swagger.json'),
-  fs = require('fs');
+  fs = require('fs'),
+  deref = require('json-schema-deref');
+
+
 var config = {
   assertionFormat: 'should',
   testModule: 'supertest',
@@ -10,9 +12,14 @@ var config = {
 //Fernando's User ID (sandbox) : "891ae771-ce5a-5014-801b-5461b5481e80"
 
 
-// Generates an array of JavaScript test files following specified configuration 
-var files = stt.testGen(swagger, config);
-console.log(files);
-files.forEach(file => {
-  fs.writeFileSync('./built-tests/' + file.name, file.test);
+deref(require('./swagger.json'), function(err, fullSchema) {
+  console.dir(fullSchema); // has the full expanded $refs
+
+  // Generates an array of JavaScript test files following specified configuration
+  var files = stt.testGen(fullSchema, config);
+  console.log(files);
+  files.forEach(file => {
+    fs.writeFileSync('./built-tests/' + file.name, file.test);
+  });
 });
+
