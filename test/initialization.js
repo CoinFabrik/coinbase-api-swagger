3b054ec725f50afa0662533925b96e8f64ebb856
ferni <fertiberti@gmail.com>
2016-04-22 07:31:44 -0300

    Set transaction_id on initialization.

--- test/initialization.js
+++ test/initialization.js
@@ -1,6 +1,8 @@
 var CoinbaseApi = require('../javascript-client/src'),
   should = require('chai').should(),
-  params = require('./param-pool');
+  params = require('./param-pool'),
+  supertest = require('supertest'),
+  api = supertest('https://api.sandbox.coinbase.com');
 
 require('dotenv').load();
 
@@ -15,7 +17,7 @@ var addressesApi = new CoinbaseApi.AddressesApi(),
 before(function(done) {
   this.timeout(8000);
 
-  var tasks = 2;
+  var tasks = 3;
   function taskDone() {
     tasks--;
     if (tasks === 0) {
@@ -29,6 +31,7 @@ before(function(done) {
     }
   }, function(error, data) {
     should.not.exist(error);
+    should.exist(data.data.id);
     params.set('account_id', data.data.id);
     addressesApi.accountsAccountIdAddressesPost(params.get('account_id'), {},
       function(error, data, response) {
@@ -36,6 +39,22 @@ before(function(done) {
         response.statusCode.should.equal(201);
         should.exist(data.data.address);
         params.set('address_id', data.data.id);
+        taskDone();
+      });
+
+    api.post('/v2/accounts/' + params.get('account_id') + '/transactions')
+      .set('Authorization', 'Bearer ' + process.env.COINBASE_ACCESS_CODE)
+      .set('Accept', 'application/json')
+      .send({
+        "type": "request",
+        "to": "someone@gmail.com",
+        "amount": "0.001",
+        "currency": "BTC"
+      })
+      .end(function(err, res) {
+        should.not.exist(err);
+        should.exist(res.body.data.status);
+        params.set('transaction_id', res.body.data.id);
         taskDone();
       });
   });
